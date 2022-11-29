dc129c432ac8036ce9d6e90a2db1d69af839a636
Ronen Saviz <ronen@oversight.co.il>
2022-11-29 13:13:30 +0200

    Update

--- myreact/src/components/newExpense/ExpenseForm.js
+++ myreact/src/components/newExpense/ExpenseForm.js
@@ -4,6 +4,27 @@ import './ExpenseForm.css';
 
 function ExpenseForm (){
 
+  const [enteredTitle,setEnterTitle]=useState('');
+  const [enteredAmount,setEnterAmount]=useState('');
+  const [enteredDate,setEnterDate]=useState('');
+  function titleChangeHendler(event)
+  {
+    setEnterTitle(event.target.value);
+    console.log(event.target.value);
+  }
+  function amountChangeHendler(event)
+  {
+    setEnterAmount(event.target.value);
+    console.log(event.target.value);
+  }
+
+  function dateChangeHendler(event)
+  {
+    setEnterDate(event.target.value);
+    console.log(event.target.value);
+  }
+
+
   return (
     <form >
       <div className='new-expense__controls'>
@@ -11,6 +32,7 @@ function ExpenseForm (){
           <label>Title</label>
           <input
             type='text'
+            onChange={titleChangeHendler}
           />
         </div>
         <div className='new-expense__control'>
@@ -19,6 +41,7 @@ function ExpenseForm (){
             type='number'
             min='0.5'
             step='0.5'
+            onChange={amountChangeHendler}
           />
         </div>
         <div className='new-expense__control'>
@@ -27,6 +50,7 @@ function ExpenseForm (){
             type='date'
             min='2019-01-01'
             max='2022-12-31'
+            onChange={dateChangeHendler}
           />
         </div>
       </div>
